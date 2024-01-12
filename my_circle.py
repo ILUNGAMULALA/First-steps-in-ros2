3208b8e1ab595b354e71339ecd17fc48349e07f7
ILUNGAMULALA <139689844+ILUNGAMULALA@users.noreply.github.com>
2024-01-12 18:23:03 +0300

    Add files via upload

---
+++ my_circle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import sys
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('py_topic_publisher_spiral')
+        self.publisher_ = self.create_publisher(Twist, 'turtle1/cmd_vel', 1)
+        timer_period = 0.5  # seconds
+        self.i = 0.0
+        self.timer_ = self.create_timer(timer_period, self.publish_message)
+
+    def publish_message(self):
+        message = Twist()
+        message.linear.x = float(sys.argv[1])+ self.i
+        message.angular.z = float(sys.argv[2]) 
+        self.get_logger().info('Sending - Linear Velocity : %f, Angular Velocity : %f' % (message.linear.x, message.angular.z))
+        self.publisher_.publish(message)
+        self.i += float(sys.argv[3])
+
+def main(args=None):
+    rclpy.init(args=args)
+    minimal_publisher = MinimalPublisher()
+    rclpy.spin(minimal_publisher)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
