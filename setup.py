d406ca4dcc81ccd046ea3a88716c7274aa770955
ILUNGAMULALA <139689844+ILUNGAMULALA@users.noreply.github.com>
2024-01-12 18:28:59 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_robot_drawer'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='daniel',
+    maintainer_email='daniel@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "test_node = my_robot_drawer.my_circle:main",
+            "turtle_controller = my_robot_drawer.turtle_controller:main"
+        ],
+    },
+)
